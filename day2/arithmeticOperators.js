776626d9cdb37c4fef3ff4d9d6402f3911b768cd
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 13:49:09 +0530

    Created file to understand Arithmetic Operators

---
+++ day2/arithmeticOperators.js
@@ -0,0 +1,15 @@
+// Javascript program to perform Arithematic Operations
+
+const x = 20;
+const y = 10;
+
+const sum = x + y;
+const difference = x - y;
+const product = x * y;
+const quotient = x / y;
+const remainder = x % y;
+console.log(x, '+', y, '=', sum);
+console.log(x, '-', y, '=', difference);
+console.log(x, '*', y, '=', product);
+console.log(x, '/', y, '=', quotient);
+console.log(x, '%', y, '=', remainder);
