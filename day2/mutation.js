4ceb0b1c4c9dd3be008805d1bc94b04c77e68a46
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 13:56:32 +0530

    Created file to understand Mutation

---
+++ day2/mutation.js
@@ -0,0 +1,10 @@
+// Javascript program to perform mutating Operations
+
+let x = 10;
+let y = 2;
+
+console.log(x, "+=", y, "=>", x += y);
+console.log(x, "-=", y, "=>", x -= y);
+console.log(x, "*=", y, "=>", x *= y);
+console.log(x, "/=", y, "=>", x /= y);
+console.log(x, "%=", y, "=>", x %= y);
