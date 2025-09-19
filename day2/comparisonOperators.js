a82ef69f811f1cb83cc656e66cf2283faef226a0
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 14:03:41 +0530

    Created file to understand Comparison Operators

---
+++ day2/comparisonOperators.js
@@ -0,0 +1,18 @@
+// Javascript program to perform Comparison Operations
+
+console.log("Sivaji is topper of the college :");
+console.log(true > false);
+
+console.log("Khasim is topper of the college :");
+console.log(true <= false);
+console.log("Khasim is topper of the University :");
+console.log(true > false);
+
+console.log("Karthik is Handome Boy");
+console.log(3 === 3);
+
+console.log("JayanthP doesn't know Javascript :");
+console.log(1 !== 1);
+
+console.log("Khasim is good at programming");
+console.log(6 <= 100);
