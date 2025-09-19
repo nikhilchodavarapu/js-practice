b20fa986fc1e4dcb0e2f981549754fbe3dcd52eb
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 17:40:05 +0530

    Update compareLetters to convert the repeated part into a loop

--- day4/practice/compareLetters.js
+++ day4/practice/compareLetters.js
@@ -4,33 +4,11 @@ let isEqual = true;
 let startIndex = 0;
 let lastIndex = input.length - 1;
 
-// comparing first and last letters in rotavator.
-if (input[startIndex] !== input[lastIndex]){
-  isEqual = false;
+while (startIndex < lastIndex) {
+  if (input[startIndex] !== input[lastIndex]) {
+    isEqual = false;
+  }
+  startIndex++;
+  lastIndex--;
 }
-startIndex++;
-lastIndex--;
-
-// comparing second and last second letters in rotavator.
-if (input[startIndex] !== input[lastIndex]){
-  isEqual = false;
-}
-startIndex++;
-lastIndex--;
-
-// comparing third and last third letters in rotavator.
-if (input[startIndex] !== input[lastIndex]){
-  isEqual = false;
-}
-startIndex++;
-lastIndex--;
-
-// comparing fourth and last fourth letters in rotavator.
-if (input[startIndex] !== input[lastIndex]){
-  isEqual = false;
-}
-startIndex++;
-lastIndex--;
-
-// Since Every letter is equal until middle one.
 console.log(isEqual);
