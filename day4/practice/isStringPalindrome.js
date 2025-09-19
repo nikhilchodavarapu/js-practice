719596650504c29ff1d2a53ac8388c80d5101b7e
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 17:47:51 +0530

    Updated isStringPalindrome file - merged compareLetters file with it

--- day4/practice/isStringPalindrome.js
+++ day4/practice/isStringPalindrome.js
@@ -1,2 +1,16 @@
-const wordToCheck = "rotavator";
-console.log(wordToCheck,"is a palindrome");
+const wordToCheck = "racecar";
+
+let isEqual = true;
+let startIndex = 0;
+let lastIndex = wordToCheck.length - 1;
+
+while (startIndex < lastIndex) {
+  if (wordToCheck[startIndex] !== wordToCheck[lastIndex]) {
+    isEqual = false;
+  }
+  startIndex++;
+  lastIndex--;
+}
+
+const suffix = isEqual ? "is a palindrome" : "is not a palindrome";
+console.log(wordToCheck, suffix);
