db0d9185a07fbb7ffe4ff855a57e68fa1fe0d74d
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-19 13:51:47 +0530

    Created file to understand Increment Decrement Operators

---
+++ day2/increment_decrement.js
@@ -0,0 +1,44 @@
+// Javascript program to perform increment and decerement operations.
+
+//Increment
+console.log();
+console.log('Increment');
+let x = 10;
+console.log("x =", x);
+
+let y = x++;
+console.log("y = x++");
+console.log("y = ", y);
+console.log("x = ", x);
+
+
+console.log();
+x = 10;
+console.log("x =", x);
+
+y = ++x;
+console.log("y = ++x");
+console.log("y = ", y);
+console.log("x = ", x);
+
+
+//Decrement
+console.log();
+console.log('Decrement');
+x = 10;
+console.log("x =", x);
+
+y = x--;
+console.log("y = x--");
+console.log("y = ", y);
+console.log("x = ", x);
+
+
+console.log();
+x = 10;
+console.log("x =", x);
+
+y = --x;
+console.log("y = --x");
+console.log("y = ", y);
+console.log("x = ", x);
