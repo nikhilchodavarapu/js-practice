70cfe71d97a5d390cbcd228de800f3f00ee447e1
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-20 13:25:50 +0530

    Update - Initialized variable

--- randomProblems/lionHunt.js
+++ randomProblems/lionHunt.js
@@ -1,2 +1,3 @@
 const savannah = "L  ZL Z";
-console.log("Minimum distance between Lion and Zebra is 0");
+let minimumDistance = 0;
+console.log("Minimum distance between Lion and Zebra is", minimumDistance);
