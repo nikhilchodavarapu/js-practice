be9b2838d589d7f7a7c5daa428dd354115fb945e
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-21 13:18:42 +0530

    Created numberOfDigits

---
+++ problemsGivenByJayanth/binarySubstringCounter/numberOfDigits.js
@@ -0,0 +1,10 @@
+const number = 973;
+let numberModified = number;
+let numberOfDigits = 0;
+
+while ( numberModified >= 1){
+    numberOfDigits++;
+    numberModified = numberModified / 10;
+}
+
+console.log("Number of Digits in", number, "is", numberOfDigits);
