9a4d6e6d39c1b6036339021d1ee7b7e04dd5203a
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-21 13:31:30 +0530

    Update binarySubstringCounter_v2 - merged decimalToBinary and substringCounter with this

--- problemsGivenByJayanth/binarySubstringCounter/binarySubstringCounter_v2.js
+++ problemsGivenByJayanth/binarySubstringCounter/binarySubstringCounter_v2.js
@@ -1,5 +1,41 @@
+// Program that counts how often a given sequence of `0`s and `1`s (a substring) appears inside the binary representation of a number.
+
 const decimalNumber = 65;
 const substring = 10;
-let occurrence = 1;
+
+// Converting Decimal to Binary.
+let binaryDigits = 0;
+let currentPlace = 1;
+let remainingQuotient = decimalNumber;
+
+while (remainingQuotient > 0){
+    let currentRemainder = remainingQuotient % 2;
+    binaryDigits += currentPlace * currentRemainder;
+    remainingQuotient = (remainingQuotient - currentRemainder) / 2;
+    currentPlace *= 10;
+}
+
+let binaryEquivalent = binaryDigits;
+console.log(decimalNumber, "=>", binaryEquivalent);
+
+// finding last digits base based on number of digits in substring to compare last digits with substring.
+let numberModified = substring;
+let lastDigitsBase = 1;
+
+while ( numberModified >= 1){
+    lastDigitsBase = lastDigitsBase * 10;
+    numberModified = numberModified / 10;
+}
+
+// counting occurence of the substring.
+let occurrence = 0;
+while ( binaryDigits !== 0 ){
+    let lastDigits = binaryDigits % lastDigitsBase;
+    if ( lastDigits === substring ){
+        occurrence++;
+    }
+    let lastDigit = binaryDigits % 10;
+    binaryDigits = (binaryDigits - lastDigit) / 10;
+}
 
 console.log("The occurrence of substring '", substring, "' in number", decimalNumber, "is", occurrence);
