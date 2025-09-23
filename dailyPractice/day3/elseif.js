87bff298dce19343ac63dae504de8292714c6172
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-23 18:51:54 +0530

    Add files via upload

---
+++ dailyPractice/day3/elseif.js
@@ -0,0 +1,11 @@
+// Javascript program to perform else-if statements
+
+let nameOfThePerson = 'Sivaji'
+
+if (nameOfThePerson === "Khasim"){
+  console.log(nameOfThePerson, "is topper of the university");
+} else if (nameOfThePerson === "Sivaji"){
+  console.log(nameOfThePerson, "is topper of the College");
+} else{
+  console.log("Karthik is Handsome Boy");
+}
---
+++ dailyPractice/day3/forLoop.js
@@ -0,0 +1,20 @@
+// Javascript program to perform while loop.
+// Program to print all odd and even numbers separately from 1 to 30.
+
+let currentNumber = 1;
+let endingNumber = 30;
+
+console.log("Odd Even")
+while (currentNumber <= endingNumber){
+  console.log(currentNumber, "  ", ++currentNumber);
+  currentNumber += 1;
+}
+
+// Javascript program to perform for loop
+// Program to print a table of number
+
+const number = 3
+
+for ( let multiplier = 1; multiplier <= 10; multiplier++){
+  console.log(number, "x", multiplier, "=", number*multiplier);
+}
