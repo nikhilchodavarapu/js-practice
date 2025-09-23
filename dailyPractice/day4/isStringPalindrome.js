776766e37d0274d860433d3b1cb632b84abdf5ac
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-23 18:56:23 +0530

    Add files via upload

---
+++ dailyPractice/day4/isStringPalindrome.js
@@ -0,0 +1,18 @@
+// Javascript program to check whether string is palindrome or not.
+
+const wordToCheck = "malayalam";
+
+let isEqual = true;
+let startIndex = 0;
+let lastIndex = wordToCheck.length - 1;
+
+while ((startIndex < lastIndex) && isEqual) {
+  if (wordToCheck[startIndex] !== wordToCheck[lastIndex]) {
+    isEqual = false;
+  }
+  startIndex++;
+  lastIndex--;
+}
+
+const suffix = isEqual ? "is a palindrome" : "is not a palindrome";
+console.log(wordToCheck, suffix);
