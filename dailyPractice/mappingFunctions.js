5b85b56e2034e7ef1cc79c5d6636329ae245f3a6
Nikhil <iamnikhilchodavarapu@gmail.com>
2025-09-30 11:47:10 +0530

    Add files via upload

---
+++ dailyPractice/mappingFunctions.js
@@ -0,0 +1,6 @@
+function isEven(x){
+  return x % 2 === 0;
+}
+
+console.log(isEven(2));
+console.log(isEven(3));
---
+++ dailyPractice/specificTaskFunctions.js
@@ -0,0 +1,10 @@
+function countNNumbers(x){
+  for (let currentNumber = 1; currentNumber <= x; currentNumber++){
+    console.log(currentNumber);
+  }
+}
+
+console.log("First 10 Natural numbers");
+countNNumbers(10);
+console.log("First 20 Natural numbers");
+countNNumbers(20);
